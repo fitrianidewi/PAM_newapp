463401b07d6602f5119fcd39a73dff73bc2a0281
fitrianidewi <fitrianidewi.2021@student.uny.ac.id>
2023-10-06 16:07:26 +0700

    new 06/10

--- app/src/main/java/com/example/new_app/MainActivity.kt
+++ app/src/main/java/com/example/new_app/MainActivity.kt
@@ -20,6 +20,15 @@ class MainActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent( this, login::class.java))
             finish()
-        },3000 )
+        },300 )
+    }
+
+    fun kesignup(view: View) {
+        setContentView(R.layout.activity_main)
+        //explicit intent
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, register::class.java))
+            finish()
+        }, 300)
     }
 }
--- app/src/main/java/com/example/new_app/register.kt
+++ app/src/main/java/com/example/new_app/register.kt
@@ -8,4 +8,13 @@ class register : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
     }
+
+    fun kesignin(view: View) {
+        setContentView(R.layout.activity_main)
+        //explicit intent
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent( this, login::class.java))
+            finish()
+        },300 )
+    }
 }
